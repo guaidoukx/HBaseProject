0d9a29bfe286ec8ed0cee29574dcd39781dfa0d4
xiangyali <172886134@qq.com>
2018-08-22 11:16:36 +0800

    to test add CompareTest

--- src/main/java/cluster/CompareTest.java
+++ src/main/java/cluster/CompareTest.java
@@ -15,10 +15,14 @@ public class CompareTest {
     private Table DBD_ID ;
     private Table phoneEnrollInfoDemo;
     private Table phoneEnrollInfo;
+    private Table ID_Timestamp;
+    private Table Timestamp_ID;
+    private Table AssistTable;
     private QueryEtc queryEtc;
 
     public CompareTest() throws IOException {
         Configuration conf = HBaseConfiguration.create();
+//        conf.set("hbase.zookeeper.quorum", "10.141.209.224, 10.141.209.223, 10.141.209.222");
         conf.set("hbase.zookeeper.quorum", "10.141.209.224");
         conf.set("hbase.zookeeper.property.clientPort", "2181");
         conf.set("hbase.master", "10.141.209.224:60000");
@@ -27,35 +31,12 @@ public class CompareTest {
         DBD_ID = connection.getTable(TableName.valueOf("DBD_ID-TS"));
         phoneEnrollInfoDemo = connection.getTable(TableName.valueOf("phoneEnrollInfoDemo"));
         phoneEnrollInfo = connection.getTable(TableName.valueOf("phoneEnrollInfo"));
+        ID_Timestamp = connection.getTable(TableName.valueOf("ID-Timestamp"));
+        Timestamp_ID = connection.getTable(TableName.valueOf("Timestamp-ID"));
+        AssistTable = connection.getTable(TableName.valueOf("AssistTable"));
         queryEtc = new QueryEtc();
     }
 
-
-
-    /* startRow-endRow VS regex mode */
-    public void One() throws IOException {
-        List<String> queryList = new ArrayList<>(Arrays.asList( //
-                "13000002944", "15869956620", "18565124452", "18301739636", "14509607268",
-                "18869941198", "13923170385", "18931777694", "15006367083", "15796649390"));
-
-        Date Start = new Date();
-        long start = Start.getTime();
-        System.out.println("start: " + start);
-
-        for (String query:queryList) {
-            queryEtc.queryInRowkeyRange(DBD_ID, query+"_t" , query+"_t9" );
-        }
-        Date Middle = new Date();
-        long middle = Middle.getTime();
-        System.out.println("Query in startRow-endRow mode: " + (middle-start));
-
-        for (String query:queryList) {
-            queryEtc.regexFilterOne(DBD_ID, query+"_t");
-        }
-        Date End = new Date();
-        long end = End.getTime();
-//        System.out.println("Query in regex mode: " + (middle-end));
-    }
 
 
     public void ColumnPrefixFilterTest(Table table, String keyword) throws IOException {
@@ -63,6 +44,8 @@ public class CompareTest {
         Scan scan = new Scan();
         ColumnPrefixFilter filter = new ColumnPrefixFilter(Bytes.toBytes(keyword));
         scan.setFilter(filter);
+        Date before = new Date();
+        System.out.println("before: " + before.getTime());
         ResultScanner resultScanner = table.getScanner(scan);
         Result result;
         int i = 1;
@@ -70,6 +53,10 @@ public class CompareTest {
             System.out.println("------- " + i++ + " --------");
             queryEtc.printResult(queryEtc.resultFormat(result));
         }
+        Date after = new Date();
+        System.out.println("after: " + after.getTime());
+        long delta = after.getTime() - before.getTime();
+        System.out.println("*****" + delta + "*****" + "\n\n");
     }
 
     public long PrefixFilterTest(Table table, String keyword) throws IOException {
@@ -98,6 +85,8 @@ public class CompareTest {
         Scan scan = new Scan();
         ColumnRangeFilter filter = new ColumnRangeFilter(Bytes.toBytes(min), true,  Bytes.toBytes(max), true);
         scan.setFilter(filter);
+        Date before = new Date();
+        System.out.println("before: " + before.getTime());
         ResultScanner resultScanner = table.getScanner(scan);
         Result result;
         int i = 1;
@@ -105,6 +94,10 @@ public class CompareTest {
             System.out.println("------- " + i++ + " --------");
             queryEtc.printResult(queryEtc.resultFormat(result));
         }
+        Date after = new Date();
+        System.out.println("after: " + after.getTime());
+        long delta = after.getTime() - before.getTime();
+        System.out.println("*****" + delta + "*****" + "\n\n");
     }
 
     public void ColumnValueFilterTest(Table table, String family, String qualifier, String value) throws IOException {
@@ -113,6 +106,8 @@ public class CompareTest {
         ColumnValueFilter filter = new ColumnValueFilter(Bytes.toBytes(family), Bytes.toBytes(qualifier),
                 CompareOperator.EQUAL, Bytes.toBytes(value));
         scan.setFilter(filter);
+        Date before = new Date();
+        System.out.println("before: " + before.getTime());
         ResultScanner resultScanner = table.getScanner(scan);
         Result result;
         int i = 1;
@@ -120,6 +115,10 @@ public class CompareTest {
             System.out.println("------- " + i++ + " --------");
             queryEtc.printResult(queryEtc.resultFormat(result));
         }
+        Date after = new Date();
+        System.out.println("after: " + after.getTime());
+        long delta = after.getTime() - before.getTime();
+        System.out.println("*****" + delta + "*****" + "\n\n");
     }
 
     public void SingleColumnValueFilterTest(Table table, String family, String qualifier, String value) throws
@@ -129,6 +128,8 @@ public class CompareTest {
         Filter filter = new SingleColumnValueFilter(Bytes.toBytes(family), Bytes.toBytes(qualifier),
                 CompareOperator.EQUAL, Bytes.toBytes(value));
         scan.setFilter(filter);
+        Date before = new Date();
+        System.out.println("before: " + before.getTime());
         ResultScanner resultScanner = table.getScanner(scan);
         Result result;
         int i = 1;
@@ -136,9 +137,13 @@ public class CompareTest {
             System.out.println("------- " + i++ + " --------");
             queryEtc.printResult(queryEtc.resultFormat(result));
         }
+        Date after = new Date();
+        System.out.println("after: " + after.getTime());
+        long delta = after.getTime() - before.getTime();
+        System.out.println("*****" + delta + "*****" + "\n\n");
     }
 
-    public void RowFilterTest(Table table, String keyword) throws IOException {
+    public long RowFilterTest(Table table, String keyword) throws IOException {
         System.out.println("This is RowFilter RegexStringComparator");
         Scan scan = new Scan();
         Filter filter = new RowFilter(CompareOperator.EQUAL, new RegexStringComparator("^"+keyword));
@@ -154,7 +159,9 @@ public class CompareTest {
         }
         Date after = new Date();
         System.out.println("after: " + after.getTime());
-        System.out.println("*****" + (after.getTime() - before.getTime()) + "*****" + "\n\n");
+        long delta = after.getTime() - before.getTime();
+        System.out.println("*****" + delta + "*****" + "\n\n");
+        return delta;
     }
 
 
@@ -190,21 +197,28 @@ public class CompareTest {
 //        compareTest.RowFilterTest(compareTest.phoneEnrollInfoDemo,"22028");
 
         /*  PrefixFilter  VS  RowFilter-Regex ^  VS  RowkeyRange */
-
-        List<String> queryList = new ArrayList<>(Arrays.asList( //
+        List<String> phoneList = new ArrayList<>(Arrays.asList( //
                 "13000002944", "15869956620", "18565124452", "18301739636", "14509607268",
                 "18869941198", "13923170385", "18931777694", "15006367083", "15796649390"));
 
+        List<String> nameList = new ArrayList<>(Arrays.asList(
+                "邹喻宋", "奚茅水", "余酆韦", "皮顾赵", "米鲁",
+                "卫郝", "禹平臧", "喻薛柏", "戴贺冯", "俞冯姚"));
+
         long sumPrefixFilter = 0L;
         long sumRowkeyRange = 0L;
-        for (String query:queryList){
-            sumPrefixFilter += compareTest.PrefixFilterTest(compareTest.DBD_ID,query+"_t");
-            sumRowkeyRange += compareTest.RowkeyRange(compareTest.DBD_ID, query+"_t", query+"_t9");
-        }
-        System.out.println("sumPrefixFilter " + sumPrefixFilter);
-        System.out.println("sumRowkeyRange " + sumRowkeyRange);
-//        compareTest.RowFilterTest(compareTest.DBD_ID,"13000002944_t");
+        long sumRowFilter = 0L;
+//        for (String query:phoneList){
+//            sumRowkeyRange += compareTest.RowkeyRange(compareTest.DBD_ID, query+"_t", query+"_t9");
+//            sumPrefixFilter += compareTest.PrefixFilterTest(compareTest.DBD_ID,query+"_t");
+//            sumRowFilter += compareTest.RowFilterTest(compareTest.DBD_ID, query);
+//        }
+//        System.out.println("sumPrefixFilter " + sumPrefixFilter);
+//        System.out.println("sumRowkeyRange " + sumRowkeyRange);
+//        System.out.println("sumRowFilter " + sumRowFilter);
+        compareTest.RowFilterTest(compareTest.DBD_ID,"13000002944_t");
 
+//        compareTest.SingleColumnValueFilterTest(compareTest.phoneEnrollInfo,"Info", "name", "米鲁");
 
 
 
