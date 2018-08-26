b2c9c7d28e6a819011eebd41f9682d1e91972fde
xiangyali <172886134@qq.com>
2018-08-26 12:37:46 +0800

    1. CompareTest 专门用于测试，测试指标在main函数之中有写说明 2. 加了HDFSXH 是星河项目，也在这里面写了

---
+++ src/main/java/HDFSXH/AttachHDFS.java
@@ -0,0 +1,74 @@
+package HDFSXH;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class AttachHDFS {
+    FileSystem fs;
+
+    public AttachHDFS() throws URISyntaxException, IOException, InterruptedException {
+        Configuration conf = new Configuration();
+//        conf.set("dfs.replication", "2");
+//        conf.set("dfs.blocksize","64m");
+        conf.set("fs.defaultFS","hdfs://10.141.209.224:9000");
+        fs = FileSystem.newInstance(conf);
+//        fs= FileSystem.get(new URI("hdfs://10.141.209.224:9000"),conf,"root");
+        System.out.println("HDFS has been attached !");
+
+    }
+
+
+    /**
+     * 用于测试 上传功能 ，源路径、目标路径都已经指定。
+     * @throws IOException 一直不清楚。
+     */
+    public void put() throws IOException {//覆盖上传
+        Path src = new Path("/Users/xiangyali/xyl.txt");
+        Path dst = new Path("/test");
+        fs.copyFromLocalFile(src, dst);
+    }
+
+    /**
+     * 用于实际应用的 上传功能
+     * @param srcString 原路径地址
+     * @param dstString HDFS目标路径地址
+     * @throws IOException 一直不清楚
+     */
+    public void put(String srcString, String dstString) throws IOException {//覆盖上传
+        Path src = new Path(srcString);
+        Path dst = new Path(dstString);
+        fs.copyFromLocalFile(src, dst);
+    }
+
+    //下载
+    public void load() throws IOException {//覆盖下载
+        Path src = new Path("G:/result2.txt");
+        Path dst = new Path("/test/result.txt");
+        fs.copyToLocalFile(dst, src);
+    }
+
+    //新建文件夹
+    public void add() throws Exception {
+        fs.mkdirs(new Path("/path"));
+    }
+
+
+    //删除文件或文件夹，bollean代表是否递归删除
+    public void delete() throws IOException {
+        fs.delete(new Path("/result.txt"),true);
+
+    }
+
+
+    public static void main(String args[]) throws InterruptedException, IOException, URISyntaxException {
+        AttachHDFS attachHDFS = new AttachHDFS();
+        attachHDFS.put();
+    }
+
+
+}
--- src/main/java/cluster/WGS.java
+++ src/main/java/cluster/WGS.java
@@ -1,171 +1,171 @@
-package cluster;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.*;
-import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.util.Bytes;
-
-import java.io.IOException;
-import java.util.*;
-
-public class WGS {
-    private static Connection connection;
-    private Table phoneEnrollInfo ;
-    private Table DBD_ID ;
-    private Table AssistTable;
-    private Table ID_Timestamp;
-
-    public WGS() throws IOException {
-        Configuration conf = HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.quorum", "10.141.209.224");
-        conf.set("hbase.zookeeper.property.clientPort", "2181");
-        conf.set("hbase.master", "10.141.209.224:60000");
-        conf.set("fs.defaultFS", "hdfs://10.141.209.224:9000");
-        connection = ConnectionFactory.createConnection(conf);
-        phoneEnrollInfo = connection.getTable(TableName.valueOf("phoneEnrollInfo"));
-        DBD_ID = connection.getTable(TableName.valueOf("DBD_ID-TS"));
-        AssistTable = connection.getTable(TableName.valueOf("AssistTable"));
-        ID_Timestamp = connection.getTable(TableName.valueOf("ID-Timestamp"));
-    }
-
-
-    List<Map<String,Map<String, Map<String,String>>>> phoneQuery(String phone)throws IOException {
-        List<Map<String,Map<String, Map<String,String>>>> resultList = new ArrayList<>();
-        int i = 1;
-        Scan scanDBD_ID = new Scan();
-        scanDBD_ID.withStartRow(Bytes.toBytes(phone+"_t"), true);
-        scanDBD_ID.withStopRow(Bytes.toBytes(phone+"_t9"), true);
-        ResultScanner resultScannerFilterList = DBD_ID.getScanner(scanDBD_ID);
-
-        Get getID = new Get(Bytes.toBytes(phone));
-        Result resultID = AssistTable.get(getID);
-        if (resultID.size() != 0){
-            byte[] ID = resultID.value();
-
-            Get getAll = new Get(ID);
-            Result resultAll = phoneEnrollInfo.get(getAll);
-            if (resultAll.size() != 0) {
-                System.out.println("------- " + i++ + " --------");
-                /* to return a list */
-                //resultList.add(this.resultFormat(resultAll));
-                /* to print result */
-                this.printResult(this.resultFormat(resultAll));
-            }else System.out.println("No ID matches the phone number " + phone);
-        }else System.out.println("There are no records about the phone number "+phone);
-
-        Result result;
-        while ((result = resultScannerFilterList.next()) != null){
-            System.out.println("------- " + i++ + " --------");
-            /* to return a list */
-            //resultList.add(this.resultFormat(result));
-            /* to print result */
-            this.printResult(this.resultFormat(result));
-        }
-        System.out.println("queryInRowkeyRange: There is no more row in this range! ");
-        resultScannerFilterList.close();
-
-        return resultList;
-    }
-
-
-
-    List<Map<String,Map<String, Map<String,String>>>> IDQuery(String ID)throws IOException {
-        List<Map<String,Map<String, Map<String,String>>>> resultList = new ArrayList<>();
-        int i = 1;
-        Scan scanID_TS = new Scan();
-        scanID_TS.withStartRow(Bytes.toBytes(ID+"-"), true);
-        scanID_TS.withStopRow(Bytes.toBytes(ID+"-9"), true);
-        ResultScanner resultScannerFilterList = ID_Timestamp.getScanner(scanID_TS);
-
-        Get getAll = new Get(Bytes.toBytes(ID));
-        Result resultAll = phoneEnrollInfo.get(getAll);
-        if (resultAll.size() != 0) {
-            System.out.println("------- " + i++ + " --------");
-            /* to return a list */
-            //resultList.add(this.resultFormat(resultAll));
-            /* to print result */
-            this.printResult(this.resultFormat(resultAll));
-        }else System.out.println("No ID matches the ID " + ID);
-
-        Result result;
-        while ((result = resultScannerFilterList.next()) != null){
-            System.out.println("------- " + i++ + " --------");
-            /* to return a list */
-            //resultList.add(this.resultFormat(result));
-            /* to print result */
-            this.printResult(this.resultFormat(result));
-        }
-        System.out.println("queryInRowkeyRange: There is no more row in this range! ");
-        resultScannerFilterList.close();
-
-        return resultList;
-    }
-
-
-    /**
-     * always used in a query method to get a format result.
-     * @param result, No need to check if it is empty or not. This step should be in query method.
-     * In a query method:
-     *       1. when there's a result, call for @resultFormat method, return format result -> triple Map.
-     *       2. otherwise, query method return @null.
-     */
-    public Map<String,Map<String, Map<String,String>>> resultFormat(Result result){
-        //
-        List<Cell> cells = result.listCells();
-        Map<String, Map<String, Map<String, String>>> resultMap = new HashMap<>();
-        cells.forEach(
-                cell -> {
-                    String qualifierString = Bytes.toString(CellUtil.cloneQualifier(cell));
-                    String valueString = Bytes.toString(CellUtil.cloneValue(cell));
-                    String familyString = Bytes.toString(CellUtil.cloneFamily(cell));
-                    String rowkeyString = Bytes.toString(CellUtil.cloneRow(cell));
-                    resultMap.putIfAbsent(rowkeyString, new HashMap<>());
-                    resultMap.get(rowkeyString).putIfAbsent(familyString, new HashMap<>());
-                    resultMap.get(rowkeyString).get(familyString).putIfAbsent(qualifierString, valueString);
-                });
-        return resultMap;
-    }
-
-
-    /**
-     * used after a query method to print the format result out.
-     * @param resultMap is either 'Map<String,Map<String, Map<String,String>>>' or @null, so check it first
-     */
-    public void printResult(Map<String,Map<String, Map<String,String>>>resultMap){
-        if(resultMap == null) {
-            System.out.println("printResult: ResultMap is empty! ");
-        }else{
-            Set<String> rowkeySet = resultMap.keySet();
-            List<String> rowkeyList = new ArrayList<>(rowkeySet);
-            rowkeyList.sort(Comparator.naturalOrder());
-            for (String rowkey : rowkeyList) {
-                System.out.println("rowkey -> " + rowkey);
-                Set<String> familySet = resultMap.get(rowkey).keySet();
-                List<String> familyList = new ArrayList<>(familySet);
-                familyList.sort(Comparator.naturalOrder());
-                for (String family : familyList) {
-                    System.out.print("\t" + "columnFamily" + " -> " + family + "\n");
-                    Set<String> qualifierSet = resultMap.get(rowkey).get(family).keySet();
-                    List<String> qualifierList = new ArrayList<>(qualifierSet);
-                    qualifierList.sort(Comparator.naturalOrder());
-                    for (String qualifier : qualifierList) {
-                        System.out.print("\t\t" + qualifier + " -> " + resultMap.get(rowkey).get(family).get(qualifier) + "\n");
-                    }
-                }
-            }
-            System.out.println();
-        }
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        WGS wgs = new WGS();
-        Date b = new Date();
-        wgs.phoneQuery("13923170385");
-        Date m = new Date();
-        System.out.println("---" + (m.getTime()-b.getTime()) + "ms ---\n");
-        wgs.IDQuery("340203197007246194");
-        Date a = new Date();
-        System.out.println("---" + (a.getTime()-m.getTime()) + "ms ---\n");
-    }
-}
+//package cluster;
+//
+//import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.hbase.*;
+//import org.apache.hadoop.hbase.client.*;
+//import org.apache.hadoop.hbase.util.Bytes;
+//
+//import java.io.IOException;
+//import java.util.*;
+//
+//public class WGS {
+//    private static Connection connection;
+//    private Table phoneEnrollInfo ;
+//    private Table DBD_ID ;
+//    private Table AssistTable;
+//    private Table ID_Timestamp;
+//
+//    public WGS() throws IOException {
+//        Configuration conf = HBaseConfiguration.create();
+//        conf.set("hbase.zookeeper.quorum", "10.141.209.224");
+//        conf.set("hbase.zookeeper.property.clientPort", "2181");
+//        conf.set("hbase.master", "10.141.209.224:60000");
+//        conf.set("fs.defaultFS", "hdfs://10.141.209.224:9000");
+//        connection = ConnectionFactory.createConnection(conf);
+//        phoneEnrollInfo = connection.getTable(TableName.valueOf("phoneEnrollInfo"));
+//        DBD_ID = connection.getTable(TableName.valueOf("DBD_ID-TS"));
+//        AssistTable = connection.getTable(TableName.valueOf("AssistTable"));
+//        ID_Timestamp = connection.getTable(TableName.valueOf("ID-Timestamp"));
+//    }
+//
+//
+//    List<Map<String,Map<String, Map<String,String>>>> phoneQuery(String phone)throws IOException {
+//        List<Map<String,Map<String, Map<String,String>>>> resultList = new ArrayList<>();
+//        int i = 1;
+//        Scan scanDBD_ID = new Scan();
+//        scanDBD_ID.withStartRow(Bytes.toBytes(phone+"_t"), true);
+//        scanDBD_ID.withStopRow(Bytes.toBytes(phone+"_t9"), true);
+//        ResultScanner resultScannerFilterList = DBD_ID.getScanner(scanDBD_ID);
+//
+//        Get getID = new Get(Bytes.toBytes(phone));
+//        Result resultID = AssistTable.get(getID);
+//        if (resultID.size() != 0){
+//            byte[] ID = resultID.value();
+//
+//            Get getAll = new Get(ID);
+//            Result resultAll = phoneEnrollInfo.get(getAll);
+//            if (resultAll.size() != 0) {
+//                System.out.println("------- " + i++ + " --------");
+//                /* to return a list */
+//                //resultList.add(this.resultFormat(resultAll));
+//                /* to print result */
+////                this.printResult(this.resultFormat(resultAll));
+//            }else System.out.println("No ID matches the phone number " + phone);
+//        }else System.out.println("There are no records about the phone number "+phone);
+//
+//        Result result;
+//        while ((result = resultScannerFilterList.next()) != null){
+//            System.out.println("------- " + i++ + " --------");
+//            /* to return a list */
+//            //resultList.add(this.resultFormat(result));
+//            /* to print result */
+////            this.printResult(this.resultFormat(result));
+//        }
+//        System.out.println("queryInRowkeyRange: There is no more row in this range! ");
+//        resultScannerFilterList.close();
+//
+//        return resultList;
+//    }
+//
+//
+//
+//    List<Map<String,Map<String, Map<String,String>>>> IDQuery(String ID)throws IOException {
+//        List<Map<String,Map<String, Map<String,String>>>> resultList = new ArrayList<>();
+//        int i = 1;
+//        Scan scanID_TS = new Scan();
+//        scanID_TS.withStartRow(Bytes.toBytes(ID+"-"), true);
+//        scanID_TS.withStopRow(Bytes.toBytes(ID+"-9"), true);
+//        ResultScanner resultScannerFilterList = ID_Timestamp.getScanner(scanID_TS);
+//
+//        Get getAll = new Get(Bytes.toBytes(ID));
+//        Result resultAll = phoneEnrollInfo.get(getAll);
+//        if (resultAll.size() != 0) {
+//            System.out.println("------- " + i++ + " --------");
+//            /* to return a list */
+//            //resultList.add(this.resultFormat(resultAll));
+//            /* to print result */
+////            this.printResult(this.resultFormat(resultAll));
+//        }else System.out.println("No ID matches the ID " + ID);
+//
+//        Result result;
+//        while ((result = resultScannerFilterList.next()) != null){
+//            System.out.println("------- " + i++ + " --------");
+//            /* to return a list */
+//            //resultList.add(this.resultFormat(result));
+//            /* to print result */
+////            this.printResult(this.resultFormat(result));
+//        }
+//        System.out.println("queryInRowkeyRange: There is no more row in this range! ");
+//        resultScannerFilterList.close();
+//
+//        return resultList;
+//    }
+//
+//
+//    /**
+//     * always used in a query method to get a format result.
+//     * @param result, No need to check if it is empty or not. This step should be in query method.
+//     * In a query method:
+//     *       1. when there's a result, call for @resultFormat method, return format result -> triple Map.
+//     *       2. otherwise, query method return @null.
+//     */
+//    public Map<String,Map<String, Map<String,String>>> resultFormat(Result result){
+//        //
+//        List<Cell> cells = result.listCells();
+//        Map<String, Map<String, Map<String, String>>> resultMap = new HashMap<>();
+//        cells.forEach(
+//                cell -> {
+//                    String qualifierString = Bytes.toString(CellUtil.cloneQualifier(cell));
+//                    String valueString = Bytes.toString(CellUtil.cloneValue(cell));
+//                    String familyString = Bytes.toString(CellUtil.cloneFamily(cell));
+//                    String rowkeyString = Bytes.toString(CellUtil.cloneRow(cell));
+//                    resultMap.putIfAbsent(rowkeyString, new HashMap<>());
+//                    resultMap.get(rowkeyString).putIfAbsent(familyString, new HashMap<>());
+//                    resultMap.get(rowkeyString).get(familyString).putIfAbsent(qualifierString, valueString);
+//                });
+//        return resultMap;
+//    }
+//
+//
+//    /**
+//     * used after a query method to print the format result out.
+//     * @param resultMap is either 'Map<String,Map<String, Map<String,String>>>' or @null, so check it first
+//     */
+//    public void printResult(Map<String,Map<String, Map<String,String>>>resultMap){
+//        if(resultMap == null) {
+//            System.out.println("printResult: ResultMap is empty! ");
+//        }else{
+//            Set<String> rowkeySet = resultMap.keySet();
+//            List<String> rowkeyList = new ArrayList<>(rowkeySet);
+//            rowkeyList.sort(Comparator.naturalOrder());
+//            for (String rowkey : rowkeyList) {
+//                System.out.println("rowkey -> " + rowkey);
+//                Set<String> familySet = resultMap.get(rowkey).keySet();
+//                List<String> familyList = new ArrayList<>(familySet);
+//                familyList.sort(Comparator.naturalOrder());
+//                for (String family : familyList) {
+//                    System.out.print("\t" + "columnFamily" + " -> " + family + "\n");
+//                    Set<String> qualifierSet = resultMap.get(rowkey).get(family).keySet();
+//                    List<String> qualifierList = new ArrayList<>(qualifierSet);
+//                    qualifierList.sort(Comparator.naturalOrder());
+//                    for (String qualifier : qualifierList) {
+//                        System.out.print("\t\t" + qualifier + " -> " + resultMap.get(rowkey).get(family).get(qualifier) + "\n");
+//                    }
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//
+//    public static void main(String[] args) throws IOException {
+//        WGS wgs = new WGS();
+//        Date b = new Date();
+//        wgs.phoneQuery("13923170385");
+//        Date m = new Date();
+//        System.out.println("---" + (m.getTime()-b.getTime()) + "ms ---\n");
+//        wgs.IDQuery("340203197007246194");
+//        Date a = new Date();
+//        System.out.println("---" + (a.getTime()-m.getTime()) + "ms ---\n");
+//    }
+//}
